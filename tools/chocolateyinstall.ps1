e153c682b74b60cc5e153ce866ed1df89be7665a
Rui Lopes <rgl@ruilopes.com>
2018-02-15 05:39:53 +0000

    import https://github.com/Tervis-Tumbler/au-packages/commit/8cfc4c2c2ceb00655e21dc90092fa33a8f46160b

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'kibana'
+$url32       = 'https://artifacts.elastic.co/downloads/kibana/kibana-5.6.4-windows-x86.zip'
+$checksum32  = '33da6553f2e97c555363b1ce08cfbfaf0e55e9640c5f9b47e575ea6e09bd5263'
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$ServiceName = 'kibana-service'
+
+Write-Host "Installing service"
+
+if ($Service = Get-Service $ServiceName -ErrorAction SilentlyContinue) {
+    if ($Service.Status -eq "Running") {
+        Start-ChocolateyProcessAsAdmin "stop $ServiceName" "sc.exe"
+    }
+    Start-ChocolateyProcessAsAdmin "delete $ServiceName" "sc.exe"
+}
+
+Start-ChocolateyProcessAsAdmin "install $ServiceName $(Join-Path $env:chocolateyPackageFolder "tools\kibana-$PackageVersion-windows-x86\bin\kibana.bat")" nssm
+Start-ChocolateyProcessAsAdmin "set $ServiceName Start SERVICE_DEMAND_START" nssm
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$PackageName="Kibana"
+$ServiceName="kibana-service"
+
+if ($Service = Get-Service $ServiceName -ErrorAction SilentlyContinue) {
+    Write-Host "Removing service."
+    if ($Service.Status -eq "Running") {
+        Start-ChocolateyProcessAsAdmin "stop $ServiceName" "sc.exe"
+    }
+    Start-ChocolateyProcessAsAdmin "delete $ServiceName" "sc.exe"
+}
